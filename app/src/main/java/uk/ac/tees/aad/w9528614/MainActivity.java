3647b5e0c7e215c431575e23fde539049d268858
Mamathareddy138 <Minnu@321>
2022-04-06 10:06:19 +0100

    Fingerprint Authentication

--- app/src/main/java/uk/ac/tees/aad/w9528614/MainActivity.java
+++ app/src/main/java/uk/ac/tees/aad/w9528614/MainActivity.java
@@ -1,27 +1,84 @@
 package uk.ac.tees.aad.w9528614;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.concurrent.Executor;
+
+public class MainActivity extends AppCompatActivity  {
+    private Executor executor;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnNext = (Button) findViewById(R.id.btn_next);
+        TextView btnNext = (TextView) findViewById(R.id.btn_next);
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
 
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "work  in progress", Toast.LENGTH_SHORT).show();
+              //  Intent  itemIntent = new Intent(MainActivity.this,MainItemActivity.class);
+               // startActivity(itemIntent);
                // Toast.makeText(MainActivity.this, "work  in progress", Toast.LENGTH_SHORT).show();
                // Toast.makeText(MainActivity.this, "work  in progress", Toast.LENGTH_SHORT).show();
             }
         });
-    }
-}
+        executor = ContextCompat.getMainExecutor(this);
+        biometricPrompt = new BiometricPrompt(MainActivity.this,
+                executor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode,
+                                              @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+                Toast.makeText(getApplicationContext(),
+                        "Authentication error: " + errString, Toast.LENGTH_SHORT)
+                        .show();
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(
+                    @NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(getApplicationContext(),
+                        "Authentication succeeded!", Toast.LENGTH_SHORT).show();
+                //Intent  itemIntent = new Intent(MainActivity.this,MainItemActivity.class);
+                //startActivity(itemIntent);
+
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+                Toast.makeText(getApplicationContext(), "Authentication failed",
+                        Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+
+        promptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric login for my app")
+                .setSubtitle("Log in using your biometric credential")
+                .setNegativeButtonText("Use account password")
+                .build();
+
+        // Prompt appears when user clicks "Log in".
+        // Consider integrating with the keystore to unlock cryptographic operations,
+        // if needed by your app.
+        ImageView biometricLoginButton = findViewById(R.id.biometric_login);
+        biometricLoginButton.setOnClickListener(view -> {
+            biometricPrompt.authenticate(promptInfo);
+        });
+    }}
